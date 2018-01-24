693c2ab45a5749c39c60a32ae8d28b7b754fa7c5
Abhi Aiyer <abhiaiyer91@gmail.com>
2018-01-23 17:53:46 -0800

    chore(exports): correct export

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 export withSubscribe from './withSubscribe';
 export { withLoadingState, withErrorState } from './withLoadingState';
+export withFragment from './withFragment';
+export withQueryData from './withQueryData';
+export withQueryComponent from './withQueryComponent';
